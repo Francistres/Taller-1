8fb277a84cd6e49a8ef2ce9f8bccd22415d9ff16
Francistres <francistapiac2006mail.com>
2024-10-20 10:33:26 -0500

    mi primer commit

---
+++ proyecto01JavaApplication5/src/Proyecto01.java
@@ -0,0 +1,6 @@
+public class Proyecto01 {
+    public static void main(String[] args) {
+        System.out.println("Ejemplo básico de Java Versionado a raves de Git manejado desde GitHub");
+    }
+    
+}
